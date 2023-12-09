236b19de2968fac87d58ffc919cdc9204aff23c7
shishir <eftekharAhmedshishir@gmail.com>
2023-12-10 03:32:27 +0600

    fixed hero title fixed

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -32,7 +32,7 @@ const HeroContent = () => {
 
         <motion.div
           variants={slideInFromLeft(0.5)}
-          className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
+          className="flex flex-col gap-6 mt-6 text-4xl font-bold text-white max-w-[600px] w-auto h-auto"
         >
           <span>
           
