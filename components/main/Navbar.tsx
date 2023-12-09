d4ec2979b1bfb21c08b557d5d789d81746c9bf8a
shishir <eftekharAhmedshishir@gmail.com>
2023-12-10 03:23:40 +0600

    nav icon add

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -24,30 +24,30 @@ const Navbar = () => {
           </span>
         </a>
 
-        <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
+        <div className="w-[600px] h-full flex flex-row items-center justify-between md:mr-20">
           <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
-            <a href="#about-me" className="cursor-pointer">
+            <a href="#about-me" className="cursor-pointer sm:text-sm ">
               About me
             </a>
-            <a href="#skills" className="cursor-pointer">
+            <a href="#skills" className="cursor-pointer sm:text-sm ">
               Skills
             </a>
-            <a href="#projects" className="cursor-pointer">
+            <a href="#projects" className="cursor-pointer sm:text-sm ">
               Projects
             </a>
           </div>
         </div>
 
         <div className="flex flex-row gap-5">
-        {/* <a href="https://www.facebook.com/Eftekhar.Ahmed20" className="cursor-pointer h-6 w-6  ">
+        <a href="https://www.facebook.com/Eftekhar.Ahmed20" className="cursor-pointer   ">
               <img src="/facebook.svg" alt="" />
             </a>
-        <a href="https://www.instagram.com/ea_shishir/" className="cursor-pointer h-6 w-6 ">
+        <a href="https://www.instagram.com/ea_shishir/" className="cursor-pointer    ">
               <img src="/instagram.svg" alt="" />
             </a>
         <a href="https://www.facebook.com/Eftekhar.Ahmed20" className="cursor-pointer h-6 w-6  ">
               <img src="/discord.svg" alt="" />
-            </a> */}
+            </a>
         </div>
       </div>
     </div>
